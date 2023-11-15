dab28658ddc605cb2db3e1b7d24175ef7d34aba4
Mochamad Nahrul <mochnahrul.wd@gmail.com>
2023-11-16 01:20:37 +0700

    fix bug when ads play

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Main.py
 import spotipy
 import json
 from flask import Flask, render_template, Response
@@ -20,19 +21,34 @@ def generate():
       current_playback = sp.current_playback()
 
       if current_playback is not None and current_playback.get("is_playing", False):
-        track_name = current_playback["item"]["name"]
-        artist_name = current_playback["item"]["artists"][0]["name"]
-        album_image_url = current_playback["item"]["album"]["images"][0]["url"]
+        if current_playback["currently_playing_type"] == "ad":
+          # Tampilkan pesan offline saat memutar iklan
+          data = {
+            "is_playing": False,
+            "ad_playing": True
+          }
+        else:
+          track_name = current_playback["item"]["name"]
+          artist_name = current_playback["item"]["artists"][0]["name"]
+          album_image_url = current_playback["item"]["album"]["images"][0]["url"]
+          data = {
+            "is_playing": True,
+            "ad_playing": False,
+            "track_name": track_name,
+            "artist_name": artist_name,
+            "album_image_url": album_image_url
+          }
       else:
         track_name = "Tidak ada lagu yang sedang diputar"
         artist_name = ""
         album_image_url = ""
-
-      data = {
-        "track_name": track_name,
-        "artist_name": artist_name,
-        "album_image_url": album_image_url
-      }
+        data = {
+          "is_playing": False,
+          "ad_playing": False,
+          "track_name": track_name,
+          "artist_name": artist_name,
+          "album_image_url": album_image_url
+        }
 
     except Exception as e:
       # Tangani kesalahan jika ada
